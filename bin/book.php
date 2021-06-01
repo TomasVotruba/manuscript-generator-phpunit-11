d499b1e5fc3184b48e0220dcf5c87abecf1f25be
Matthias Noback <matthiasnoback@gmail.com>
2021-06-01 13:22:29 +0200

    Set up bin/book.php as bin script

--- bin/book.php
+++ bin/book.php
@@ -4,7 +4,22 @@ declare(strict_types=1);
 use ManuscriptGenerator\Cli\BookCliApplication;
 use ManuscriptGenerator\Cli\GenerateManuscriptCommand;
 
-require __DIR__ . '/../vendor/autoload.php';
+$potentialAutoloadFiles = [
+    __DIR__ . '/../vendor/autoload.php', // in this project
+    __DIR__ . '/../autoload.php', // in a project where this library is installed
+];
+$autoloaderLoaded = false;
+
+foreach ($potentialAutoloadFiles as $file) {
+    if (file_exists($file)) {
+        require $file;
+        $autoloaderLoaded = true;
+    }
+}
+
+if (!$autoloaderLoaded) {
+    throw new RuntimeException('Could not find autoload.php');
+}
 
 $application = new BookCliApplication(
     [
