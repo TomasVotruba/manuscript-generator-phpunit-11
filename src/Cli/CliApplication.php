f3c8262b3169d1135784fd0b373a1320b26f141b
Matthias Noback <matthiasnoback@gmail.com>
2021-06-01 13:28:59 +0200

    Turn into a single-command app

---
+++ src/Cli/CliApplication.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ManuscriptGenerator\Cli;
+
+use Symfony\Component\Console\Application;
+
+final class CliApplication extends Application
+{
+    public function __construct()
+    {
+        parent::__construct('Manuscript Generator', '1.0');
+
+        $command = new GenerateManuscriptCommand();
+        $this->add($command);
+        $this->setDefaultCommand($command->getName(), true);
+    }
+}
