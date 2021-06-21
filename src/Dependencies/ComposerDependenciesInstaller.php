06daae338b3bd8c806db8d5a541759819307ace7
Matthias Noback <matthiasnoback@gmail.com>
2021-06-21 11:43:19 +0200

    Use install for Composer deps

--- src/Dependencies/ComposerDependenciesInstaller.php
+++ src/Dependencies/ComposerDependenciesInstaller.php
@@ -29,7 +29,7 @@ final class ComposerDependenciesInstaller
             $composer = new Process(
                 [
                     'composer', // @TODO make configurable
-                    'update',
+                    'install',
                 ],
                 $workingDir
             );
