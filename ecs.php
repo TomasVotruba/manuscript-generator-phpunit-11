abc417d0794ca5c4a12f6025270f04d006b845ed
Tomas Votruba <tomas.vot@gmail.com>
2021-12-08 11:22:09 +0300

    ecs - enable parallel

--- ecs.php
+++ ecs.php
@@ -15,6 +15,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::PATHS, [__DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php']);
 
     $parameters->set(Option::CACHE_DIRECTORY, getcwd() . '/cache/ecs');
+    $parameters->set(Option::PARALLEL, true);
 
     $containerConfigurator->import(SetList::CONTROL_STRUCTURES);
     $containerConfigurator->import(SetList::PSR_12);
